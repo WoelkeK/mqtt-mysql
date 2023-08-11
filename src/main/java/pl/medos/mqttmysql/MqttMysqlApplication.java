a53d171f462d5c7e37803138cdc2fce9bf33b2f9
Krzysztof <krzysztof@woelke.pl>
2023-08-11 12:08:30 +0200

    init structure for mqtt-mysql data login project

---
+++ src/main/java/pl/medos/mqttmysql/MqttMysqlApplication.java
@@ -0,0 +1,13 @@
+package pl.medos.mqttmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/medos/mqttmysql/MqttMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.medos.mqttmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
